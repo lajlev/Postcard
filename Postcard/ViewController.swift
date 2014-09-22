5e608927d3b120d98d6b4b1ec53b2cde90fca4ef
Michael Lajlev <mla@mobilethink.com>
2014-09-22 10:16:59 +0200

    Herping the derp derp (silly scoping error)

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var nameLabel: UILabel!
+  
   @IBOutlet weak var messageLabel: UILabel!
     
   @IBOutlet weak var enterNameTextField: UITextField!
@@ -30,13 +32,22 @@ class ViewController: UIViewController {
   }
 
   @IBAction func sendMailButtonPressed(sender: UIButton) {
-	   // Code will evaluate when we press the button
-  	 messageLabel.hidden = false
-  	 messageLabel.text = enterMessageTextField.text
-  	 enterMessageTextField.text = ""
-		 enterMessageTextField.resignFirstResponder()
-  	 messageLabel.textColor = UIColor.redColor()
-  	 mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+    // Code will evaluate when we press the button
+    messageLabel.hidden = false
+    messageLabel.text = enterMessageTextField.text
+    enterMessageTextField.text = ""
+    enterMessageTextField.resignFirstResponder()
+    messageLabel.textColor = UIColor.redColor()
+    
+    nameLabel.hidden = false
+    nameLabel.text = enterNameTextField.text
+    nameLabel.textColor = UIColor.blueColor()
+    enterNameTextField.text = ""
+    
+    
+    
+    mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+    
   }
 
 
